3520f8abdaf6b7401f45a69b2ff4ba74cf379a04
viettuanpham1991 <viettuanpham1991@gmail.com>
2014-05-06 22:17:27 +0000

    Nouveauté : script du shield

---
+++ Unity/test/Assets/Player/ShieldUp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldUp : MonoBehaviour {
+
+	private hudHandler hud = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (hud == null) 
+		{
+			if (GameObject.FindGameObjectsWithTag("HUD") != null)
+			{
+				hud = GameObject.FindGameObjectWithTag("HUD").GetComponent<hudHandler>();
+				
+			}
+		}
+
+		if (hud.getInvincible ()) 
+		{
+			this.renderer.enabled = true;
+		} 
+		else 
+		{
+			this.renderer.enabled = false;
+		}
+	}
+}
