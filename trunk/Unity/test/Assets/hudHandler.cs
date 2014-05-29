0e06c0bd9380e6872059a2ed23fd3fc59c46b325
quprevost@gmail.com <quprevost@gmail.com@7700dab2-12e4-384b-505c-07cfd2b0133b>
2014-05-29 19:35:12 +0000

    Nouvelle Jauge + ajout de Quitter dans lemenu pause

--- trunk/Unity/test/Assets/hudHandler.cs
+++ trunk/Unity/test/Assets/hudHandler.cs
@@ -17,6 +17,8 @@ public class  hudHandler : MonoBehaviour {
 
     //public GUIText health;
     public GUIText score;
+    private GUITexture health;
+    private GUITexture energy;
 
 
 	public GameObject server;
@@ -25,18 +27,19 @@ public class  hudHandler : MonoBehaviour {
 
     // GUI variable
 
-    private Rect healthRect;
-    private Rect powerRect;
+//    private Rect healthRect;
+//    private Rect powerRect;
 
     private int jaugeWidth = 40;
 
     private int jaugeHeight;
 
     private int topIndent =  50;
-
-    private int hIndent =50;
-
-    GUIStyle style;
+    private int leftIndent =50;
+    private int healthTopIndent;
+    private int healthLeftIndent;
+    private int energyLeftIndent;
+    private int energyTopIndent;
 
     public Texture vert;
     public Texture rouge;
@@ -61,9 +64,23 @@ public class  hudHandler : MonoBehaviour {
 
 
 	void Start () {
-        //health.text = intHealth.ToString();
+        
+        // on recupère la hauteur de la jauge
+        jaugeHeight =  Screen.height +- 100;
+        healthTopIndent = Screen.height / 2 - topIndent;
+        healthLeftIndent = -Screen.width / 2 + leftIndent;
+        energyTopIndent = Screen.height / 2 - topIndent;
+        energyLeftIndent = Screen.width / 2 - 2*leftIndent;
+        
+        // on iniitialise les affichages à l'écran
         score.text = intScore.ToString();
-		//energy.text = floatEnergy.ToString ();
+        health = GameObject.Find("Health").GetComponent<GUITexture>();
+        energy = GameObject.Find("Energy").GetComponent<GUITexture>();
+        health.pixelInset = new Rect( healthLeftIndent, healthTopIndent, jaugeWidth, -jaugeHeight);
+        energy.pixelInset = new Rect(energyLeftIndent, energyTopIndent, jaugeWidth, -jaugeHeight);
+        energy.texture = gris;
+        health.texture = vert;
+
 		invincible = false;
         jaugeHeight = Screen.height - 100;
 		score.pixelOffset = new Vector2 (Screen.width / 2, Screen.height - 40 );
@@ -75,11 +92,14 @@ public class  hudHandler : MonoBehaviour {
 	void Update () 
     {	
 
+        // on regarde si on doit activer l'invincibilité
 
+        
 		if (withAndroid) 
 		{
+            // si on est sous android, on récupère la feuille de données
 			data = server.GetComponent<ServerHandler>().getData();
-			//Debug.Log(data.shield);
+			
 			if ( data.shield && withAndroid && invincible == false && floatEnergy > 100)
 			{
 				StartCoroutine (invincibility());
@@ -121,7 +141,41 @@ public class  hudHandler : MonoBehaviour {
 		}
 		
 
+        // on met à jour le score à l'écran
+
 		score.text = intScore.ToString ();
+
+        // on met à jour les jauge à l'écran 
+
+        if (intHealth > 20)
+        {
+            health.texture = vert;
+            health.pixelInset = new Rect(healthLeftIndent, healthTopIndent, jaugeWidth, -jaugeHeight * intHealth /100);
+        }
+        else
+        {
+            health.texture = rouge;
+            health.pixelInset = new Rect(healthLeftIndent, healthTopIndent, jaugeWidth, -jaugeHeight * intHealth /100);
+        }
+
+        if (floatEnergy < 100)
+        {
+            energy.texture = gris;
+            energy.pixelInset = new Rect(energyLeftIndent, energyTopIndent, jaugeWidth, -jaugeHeight * floatEnergy/150);
+        }
+        else
+        {
+            if (floatEnergy == 150)
+            {
+                energy.texture = orange;
+                energy.pixelInset = new Rect(energyLeftIndent, energyTopIndent, jaugeWidth, -jaugeHeight * floatEnergy / 150);
+            }
+            else
+            {
+                energy.texture = jaune;
+                energy.pixelInset = new Rect(energyLeftIndent, energyTopIndent, jaugeWidth, -jaugeHeight * floatEnergy / 150);
+            }
+        }
 
         // Execution dépendant de l'état de l'energie;
 
@@ -214,30 +268,7 @@ public class  hudHandler : MonoBehaviour {
 	{
 
 
-        if (intHealth >20)
-        {
-            GUI.Box(new Rect(hIndent, topIndent, jaugeWidth, jaugeHeight * intHealth/100), vert); 
-        }
-        else 
-        {
-            GUI.Box(new Rect(hIndent, topIndent, jaugeWidth, jaugeHeight * intHealth / 100), rouge);
-        }
-	
-        if (floatEnergy < 100)
-        {
-            GUI.Box(new Rect(Screen.width - 2*hIndent, topIndent, jaugeWidth, jaugeHeight * floatEnergy / 150), gris); 
-        }
-        else
-        {
-            if (floatEnergy  == 150)
-            {
-                GUI.Box(new Rect(Screen.width - 2 * hIndent, topIndent, jaugeWidth, jaugeHeight * floatEnergy / 150), orange); 
-            }
-            else
-            {
-                GUI.Box(new Rect(Screen.width - 2 * hIndent, topIndent, jaugeWidth, jaugeHeight * floatEnergy / 150), jaune);
-            }
-        }
+
     }
 
 }
